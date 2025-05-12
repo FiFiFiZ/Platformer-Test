d08cbf69bb2d33ac71874a66c30dc50c01a373e6
FiFiZ <Fifis.luigi@gmail.com>
2025-05-12 23:41:08 +0200

    wip scrolling, added ground assets

--- assets.py
+++ assets.py
@@ -5,7 +5,7 @@ class Assets:
         self.sprites = {}
         to_load = [ # Load Images from the Images folder
             ("player", ""),
-            ("ground", "")
+            ("ground", "add_n", 9)
         ]  # names with special second (or more) item get special treatment in loading (like loading both 0 and 1 name-ending variants)
 
         for i in range (len(to_load)):
@@ -14,12 +14,12 @@ class Assets:
                 # add_n special (starts at 1):
                 image_path += f"{to_load[i][0]}"
                 for j in range (to_load[i][2]):
-                    self.sprites[to_load[i][0] + str(j+1)] = pygame.image.load(image_path + str(j+1) + ".png")
+                    self.sprites[to_load[i][0] + str(j)] = pygame.image.load(image_path + str(j) + ".png").convert()
             else:
                 image_path += f"{to_load[i][0]}.png"
                 if to_load[i][1] == "keep_transparency":
                     # keep_transparency:
-                    self.sprites[to_load[i][0]] = pygame.image.load(image_path)
+                    self.sprites[to_load[i][0]] = pygame.image.load(image_path).convert()
                 else:
                     # standard load:
                     self.sprites[to_load[i][0]] = pygame.image.load(image_path)
--- main.py
+++ main.py
@@ -23,6 +23,60 @@ class Game:
         self.clock = pygame.time.Clock()
         self.run = True
 
+        self.player_x = 60
+        self.player_y = 60
+        self.player_xs = 0
+        self.player_ys = 0
+
+        self.ground_positions = []
+        y = -72
+        for i in range(3):
+            x = -128
+            for n in range(3):
+                self.ground_positions.append((x,y))
+                x += 128
+            y -= 72
+
+    def check_collision(self):
+        player_mask = pygame.mask.from_surface(self.sprites["player"])
+        ground_mask = pygame.mask.from_surface(self.sprites["ground0"])
+
+        for i in range(9):
+            # overlap = player_mask.overlap(ground_mask, ((self.player_xs), (self.player_ys)))
+            overlap = player_mask.overlap(ground_mask, ((self.player_xs), (self.player_ys)))
+
+        # player_mask.invert()
+        # ground_mask.invert()
+
+        player_mask = player_mask.to_surface(setcolor=(255, 255, 255, 255), unsetcolor=(0, 0, 0, 255))
+        ground_mask = ground_mask.to_surface(setcolor=(255, 255, 255, 255), unsetcolor=(0, 0, 0, 255))
+
+        # self.screen.blit(ground_mask, (0, 0))
+        # self.screen.blit(player_mask, (self.player_x, self.player_y))
+
+
+        # overlap = ground_mask.overlap(player_mask, ((self.player_xs), (self.player_ys)))
+        if overlap == None:
+            pass
+        else:
+            print(overlap)
+
+        pass
+
+    def player_move(self):
+        keydir = 0
+        if self.key_held[pygame.K_LEFT] == True:
+            keydir -= 1
+        if self.key_held[pygame.K_RIGHT] == True:
+            keydir += 1
+        self.player_xs += keydir
+        self.player_xs *= 0.98
+
+        self.check_collision()
+
+        self.player_x += self.player_xs
+        self.player_y += self.player_ys
+
     def game_run(self):
         while self.run:
 
@@ -31,10 +85,19 @@ class Game:
             self.key_held = pygame.key.get_pressed()
             self.screen.fill((255,255,255))
 
-            placeholder_player = pygame.transform.scale_by(self.sprites["player"], 5)
-            placeholder_ground = pygame.transform.scale_by(self.sprites["ground"], 5)
-            self.screen.blit(placeholder_ground, (0, 0))
-            self.screen.blit(placeholder_player, (60, 60))
+            factor = 1
+            self.sprites["player"] = pygame.transform.scale(self.sprites["player"], (8*factor, 8*factor))
+            self.sprites["ground0"] = pygame.transform.scale(self.sprites["ground0"], (128*factor, 72*factor))
+            self.player_x, self.player_y = pygame.mouse.get_pos()
+
+            self.player_move()
+            self.check_collision()
+
+            # self.screen.blit(self.sprites["ground0"], (0, 0))
+            self.screen.blit(self.sprites["player"], (self.player_x, self.player_y))
+            for i in range(9):
+                self.screen.blit(self.sprites[f"ground{i}"], self.ground_positions[i])
+
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
