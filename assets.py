134b4fd13fa942ba5942d5d272154bfe777f9523
FiFiZ <Fifis.luigi@gmail.com>
2025-05-21 19:59:24 +0200

    loadable player hitboxes

--- assets.py
+++ assets.py
@@ -5,7 +5,8 @@ class Assets:
         self.sprites = {}
         to_load = [ # Load Images from the Images folder
             ("player", ""),
-            ("ground", "add_n", 9)
+            ("ground", "add_n", 9),
+            ("playerh ", "add_list", ["bottom", "top", "right", "left"])
         ]  # names with special second (or more) item get special treatment in loading (like loading both 0 and 1 name-ending variants)
 
         self.SCREEN_WIDTH = pygame.display.Info().current_w
@@ -19,8 +20,12 @@ class Assets:
             if to_load[i][1] == "add_n":
                 # add_n special (starts at 1):
                 image_path += f"{to_load[i][0]}"
-                for j in range (to_load[i][2]):
-                    self.sprites[to_load[i][0] + str(j)] = pygame.image.load(image_path + str(j) + ".png")
+                self.add_n(to_load[i], image_path)
+
+            elif to_load[i][1] == "add_list":
+                # concatenate string with every list element
+                image_path += f"{to_load[i][0]}"
+                self.add_list(to_load[i], image_path)
             else:
                 image_path += f"{to_load[i][0]}.png"
                 if to_load[i][1] == "keep_transparency":
@@ -31,3 +36,12 @@ class Assets:
                     self.sprites[to_load[i][0]] = pygame.image.load(image_path)
 
                     # self.sprites[to_load[i]].colorkey((153, 102, 255))
+
+    def add_n(self, to_load, image_path):
+        for j in range (to_load[2]):
+            self.sprites[to_load[0] + str(j)] = pygame.image.load(image_path + str(j) + ".png")
+
+    def add_list(self, to_load, image_path):
+        for items in to_load[2]:
+            self.sprites[to_load[0] + str(items)] = pygame.image.load(image_path + str(items) + ".png")
+
